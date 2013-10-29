a6af54f9ff918f6a7ce19c299997a9b4c72b7d19
Patrick Hund <wiekatz@googlemail.com>
2013-10-29 11:03:33 -0400

    added 2 more demos

---
+++ js/demo3.js
@@ -0,0 +1,27 @@
+$('document').ready(function () {
+    var snap = Snap('#demo');
+   
+    Snap.load('img/mobail-icon.svg', function (button) {
+        snap.append(button);
+
+        var phone = Snap.select('#icon');
+        var overlay = Snap.select('#overlay');
+        var handler;
+
+        overlay.hover(function () {
+            console.log('rein');
+            handler = window.setInterval(function () {
+                phone.animate({
+                    transform: 't2r0'
+                }, 20, null, function () {
+                    phone.animate({
+                        transform: 't-2r0'
+                    }, 20);
+                });
+            }, 60);
+        }, function () {
+            console.log('raus');
+            window.clearInterval(handler);
+        });
+    });
+});
